f1987e941e5c532329fcd4bb562840734bef23fb
syz <suyizhen0911@gmail.com>
2021-03-24 21:43:07 +0900

    add  collapseComments

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,7 +1,6 @@
-
 // コメントする
-function commit(targetID,content,type) {
-    if (!content){
+function commit(targetID, content, type) {
+    if (!content) {
         alert("空欄のコメントをコミットすることができません。");
         return;
     }
@@ -23,7 +22,7 @@ function commit(targetID,content,type) {
                     let isAccepted = confirm(response.message);
                     if (isAccepted) {
                         window.open("https://github.com/login/oauth/authorize?client_id=115285b7216b4c2397a1&redirect_uri=http://localhost:8080/callback&scope=user&state=1");
-                        window.localStorage.setItem("closable","true");
+                        window.localStorage.setItem("closable", "true");
                     }
                 } else {
                     alert(response.message)
@@ -34,22 +33,61 @@ function commit(targetID,content,type) {
         dataType: "json"
     });
 }
+
 function post() {
     let parentId = $("#question_id").val();
     let content = $("#comment_content").val();
-    commit(parentId,content,1);
+    commit(parentId, content, 1);
 }
+
 function comment(e) {
     let commentId = e.getAttribute("data-id");
     let content = $("#input-" + commentId).val();
-    commit(commentId,content,2);
+    commit(commentId, content, 2);
 }
 
 //二級コメントを展開する
-function collapseComments(e){
+function collapseComments(e) {
     let id = e.getAttribute("data-id");
     let btn = $("#commentBtn-" + id);
     let comment = $("#comment-" + id);
+    if (comment.children().length == 1) {
+        $.getJSON("/comment/" + id, function (data) {
+            $.each(data.data.reverse(), function (index, comment) {
+                let subCommentContainer = $("#comment-" + id);
+                let mediaLeftElement = $("<div/>", {
+                    "class": "media-left"
+                }).append($("<img/>", {
+                    "class": "media-object img-rounded",
+                    "src": comment.user.avatarUrl
+                }));
+
+                let mediaBodyElement = $("<div/>", {
+                    "class": "media-body"
+                }).append($("<h5/>", {
+                    "class": "media-heading",
+                    "html": comment.user.name
+                })).append($("<div/>", {
+                    "html": comment.content
+                })).append($("<div/>", {
+                    "class": "menu"
+                }).append($("<span/>", {
+                    "class": "pull-right",
+                    "html": moment(comment.gmtCreate).format('YYYY-MM-DD')
+                })));
+
+                let mediaElement = $("<div/>", {
+                    "class": "media"
+                }).append(mediaLeftElement).append(mediaBodyElement);
+
+                let commentElement = $("<div/>", {
+                    "class": "col-lg-12 col-md-12 col-sm-12 col-xs-12 comments"
+                }).append(mediaElement);
+
+                subCommentContainer.prepend(commentElement)
+            });
+        });
+    }
     comment.toggleClass("in");
     btn.toggleClass("active");
 }
